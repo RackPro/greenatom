90b999bc4ed9fff31be6d840ee2961cf14935a99
RackPro <77266716+RackPro@users.noreply.github.com>
2024-05-07 20:57:13 +0000

    23:57

--- Person.js
+++ Person.js
@@ -1,8 +1,42 @@
+let obj={
+    base: {
+        name: "",
+        max_hp: 0,
+        hp: 0,
+        reg_hp: 0,
+        max_mana: 0,
+        mana: 0,
+        reg_mana: 0,
+        fletta: 0, //Для прокачки дерева особых навыков.
+        phys_damage_ot: 0,
+        phys_damage_do: 0,
+        magic_damage_ot: 0,
+        magic_damage_do: 0,
+        phiz_armor: 0,
+        magic_armor: 0,
+        chance_uklon: 0,
+        chance_block: 0,
+        chance_crit: 0,
+        damage_from_crit: 0,
+        skat: 0,
+        vampirizm:0,
+    },
+
+    stati:{
+        intelect: 0, // мана, реген маны.
+        lovkost: 0, // скорость атаки, урон от и до
+        sila: 0, // физ урон от и до
+        mudrost: 0,
+    }
+}
+
 class Person{    
-    constructor(name, hp,mana,damageOt, damageDo, fizzach, uklon, block, vampirizm, skat){
+    constructor(name, hp, regHp, mana, regMana, damageOt, damageDo, fizzach, uklon, block, vampirizm, skat){
         this.name = name;
         this.hp = hp;
+        this.regHp = regHp
         this.mana = mana;
+        this.regMana = regMana;
         this.damageOt = damageOt;
         this.damageDo = damageDo;
         this.fizzach = fizzach;
--- index1.js
+++ index1.js
@@ -1,13 +1,21 @@
 const Person = require("./Person");
+
 //                              hp   om    ot   do  zach ukl blc vamp skat
-let pers1 = new Person("Sasha", 4000, 100, 110, 240, 400, 0, 20, 27,  1500);
-let pers2 = new Person("Lexa",  2000, 50,  200, 400,  50, 20, 0, 12,  550);
+let pers1 = new Person("Sasha", 4000, 40, 100, 10, 110, 240, 400, 0, 20, 27,  1500);
+let pers2 = new Person("Lexa",  2000, 40, 50, 10,  200, 400,  50, 20, 0, 12,  550);
+let pers3 = new Person("BOSS",  20000, 40, 50, 10,  300, 500,  50, 20, 0, 12,  550);
 
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
 
+function regen(obj){
+    setInterval(()=>{
+        obj.hp+=obj.regHp; //максимальная мана
+        obj.mana+=obj.regMana;
+    },1000);
+}
 function attack(obj1, obj2){
     setInterval(()=>{
         let damage = randomInteger(obj1.damageOt, obj1.damageDo); //Наносимый дамаг
@@ -24,5 +32,7 @@ function attack(obj1, obj2){
     },obj1.skat);
 }
 
-attack(pers1, pers2);
-attack(pers2, pers1);
+attack(pers1 ,pers3);
+attack(pers2 ,pers3);
+
+attack(pers3, pers1);
